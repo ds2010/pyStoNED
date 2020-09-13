46d3326ba3d9868d19101669504c1f0bc57e6d22
JulianATA <julianisabear@gmail.com>
2020-09-13 17:24:22 +0800

    refactor(ICQER): Refactor ICER model based on CER class and ICNLS class

--- pystoned/ICQER.py
+++ pystoned/ICQER.py
@@ -25,3 +25,25 @@ class ICQR(ICNLS.ICNLS, CQER.CQR):
                                                          self.__model__.I,
                                                          rule=self._ICNLS__isotonic_afriat_rule(),
                                                          doc='isotonic afriat inequality')
+
+class ICER(ICNLS.ICNLS, CQER.CER):
+    """Isotonic convex expectile regression (ICER)"""
+    def __init__(self, y, x, tau, cet='addi', fun='prod', rts='vrs'):
+        """
+            y : Output variable
+            x : Input variables
+            cet  = "addi" : Additive composite error term
+                 = "mult" : Multiplicative composite error term
+            fun  = "prod" : Production frontier
+                 = "cost" : Cost frontier
+            rts  = "vrs"  : Variable returns to scale
+                 = "crs"  : Constant returns to scale
+        """
+        CQER.CER.__init__(self, y, x, tau, cet, fun, rts)
+
+        self._ICNLS__pmatrix = self._ICNLS__binaryMatrix()
+        self.__model__.afriat_rule.deactivate()
+        self.__model__.isotonic_afriat_rule = Constraint(self.__model__.I,
+                                                         self.__model__.I,
+                                                         rule=self._ICNLS__isotonic_afriat_rule(),
+                                                         doc='isotonic afriat inequality')
