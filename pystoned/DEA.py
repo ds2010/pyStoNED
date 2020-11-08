7b0520931514b6a0acd49fbedf4266d012dfa09b
JulianATA <julianisabear@gmail.com>
2020-11-08 23:38:36 +0800

    feat(DEA): Implement DDF and reference model

--- pystoned/DEA.py
+++ pystoned/DEA.py
@@ -10,7 +10,7 @@ class DEA:
     """
     Data Envelopment Analysis (DEA)
     """
-    def __init__(self, y, x, orient, rts):
+    def __init__(self, y, x, orient, rts, yref=None, xref=None):
         """
         orient  = "io" : input orientation
                 = "oo" : output orientation
@@ -18,20 +18,10 @@ class DEA:
                 = "crs": constant returns to scale
         """
         # TODO(error/warning handling): Check the configuration of the model exist
-        self.x = x.tolist()
-        self.y = y.tolist()
+        self.x = self.__to_2d_list(x.tolist())
+        self.y = self.__to_2d_list(y.tolist())
         self.orient = orient
         self.rts = rts
-
-        if type(self.x[0]) != list:
-            self.x = []
-            for x_value in x.tolist():
-                self.x.append([x_value])
-
-        if type(self.y[0]) != list:
-            self.y = []
-            for y_value in y.tolist():
-                self.y.append([y_value])
 
         # Initialize DEA model
         self.__model__ = ConcreteModel()
@@ -58,6 +48,15 @@ class DEA:
         # Optimize model
         self.optimization_status = 0
         self.problem_status = 0
+
+    def __to_2d_list(self, l):
+        """If the given list l is 1d list, return its 2d adaption"""
+        if type(l[0]) != list:
+            r = []
+            for value in l:
+                r.append([value])
+            return r
+        return l
 
     def __objective_rule(self):
         """Return the proper objective function"""
@@ -151,3 +150,118 @@ class DEA:
         lamda = list(self.__model__.lamda[:].value)
         return np.asarray(lamda)
 
+class DEADDF(DEA):
+    def __init__(self,  y, x, b=None, gy=[1], gx=[1], gb=None, rts="vrs", yref=None, xref=None,bref=None):
+        """
+            y : Output variables
+            x : Input variables
+            b : Undesirable output variables
+            gy : Output directional vector
+            gx : Input directional vector
+            gb : Undesirable output directional vector
+            rts     = "vrs": variable returns to scale
+                    = "crs": constant returns to scale
+            yref : the reference point of y
+            xref : the reference point of x
+            bref : the reference point of b
+        """
+        # Initialize DEA model
+        self.__model__ = ConcreteModel()
+
+        self.x = self._DEA__to_2d_list(x.tolist())
+        self.y = self._DEA__to_2d_list(y.tolist())
+        self.rts = rts
+        self.gy = self.__to_1d_list(gy)
+        self.gx = self.__to_1d_list(gx)
+        self.__undesirable_output = False
+        self.__reference = False
+
+        if type(b) != type(None):
+            self.__undesirable_output = True
+            self.b = self._DEA__to_2d_list(b.tolist())
+            self.gb = self.__to_1d_list(gb)
+        
+        if type(yref) != type(None):
+            self.__reference = True
+            self.yref = self._DEA__to_2d_list(yref)
+            self.xref = self._DEA__to_2d_list(xref)
+            if self.__undesirable_output:
+                self.bref = self._DEA__to_2d_list(bref)
+            self.__model__.R = Set(initialize=range(len(self.yref)))
+
+        # Initialize sets
+        self.__model__.I = Set(initialize=range(len(self.y)))
+        self.__model__.J = Set(initialize=range(len(self.x[0])))
+        self.__model__.K = Set(initialize=range(len(self.y[0])))
+        if self.__undesirable_output:
+            self.__model__.L = Set(initialize=range(len(self.b[0])))
+
+        # Initialize variable
+        self.__model__.theta = Var(self.__model__.I, doc='directional distance')
+        if self.__reference:
+            self.__model__.lamda = Var(self.__model__.I, self.__model__.R, bounds=(0.0,None), doc='intensity variables')
+        else:
+            self.__model__.lamda = Var(self.__model__.I, self.__model__.I, bounds=(0.0,None), doc='intensity variables')
+
+        # Setup the objective function and constraints
+        self.__model__.objective = Objective(rule=self._DEA__objective_rule(), sense=maximize, doc='objective function')
+        self.__model__.input = Constraint(self.__model__.I, self.__model__.J, rule=self.__input_rule(), doc='input constraint')
+        self.__model__.output = Constraint(self.__model__.I, self.__model__.K, rule=self.__output_rule(), doc='output constraint')
+        
+        if self.__undesirable_output:
+            self.__model__.undesirable_output = Constraint(self.__model__.I, self.__model__.L, rule=self.__undesirable_output_rule(), doc='undesirable output constraint')
+        
+        if self.rts == "vrs":
+            self.__model__.vrs = Constraint(self.__model__.I, rule=self.__vrs_rule(), doc='various return to scale rule')
+
+        # Optimize model
+        self.optimization_status = 0
+        self.problem_status = 0
+
+    def __to_1d_list(self, l):
+        if type(l) == int or type(l) == float:
+            return [l]
+        return l
+
+    def __input_rule(self):
+        """Return the proper input constraint"""
+        if self.__reference == False:
+            def input_rule(model, o, j):
+                return self.x[o][j] - model.theta[o]*self.gx[j] >= sum(model.lamda[o, i] * self.x[i][j] for i in model.I)
+            return input_rule
+        else:
+            def input_rule(model, o, j):
+                return self.x[o][j] - model.theta[o]*self.gx[j] >= sum(model.lamda[o, r] * self.xref[r][j] for r in model.R)
+            return input_rule
+
+    def __output_rule(self):
+        """Return the proper output constraint"""
+        if self.__reference == False:
+            def output_rule(model, o, k):
+                return self.y[o][k] + model.theta[o]*self.gy[k] <= sum(model.lamda[o, i] * self.y[i][k] for i in model.I)
+            return output_rule
+        else:
+            def output_rule(model, o, k):
+                return self.y[o][k] + model.theta[o]*self.gy[k] <= sum(model.lamda[o, r] * self.yref[r][k] for r in model.R)
+            return output_rule
+
+    def __undesirable_output_rule(self):
+        """Return the proper undesirable output constraint"""
+        if self.__reference == False:
+            def undesirable_output_rule(model, o, l):
+                return self.b[o][l] - model.theta[o]*self.gb[l] == sum(model.lamda[o, i] * self.b[i][l] for i in model.I)
+            return undesirable_output_rule
+        else:
+            def undesirable_output_rule(model, o, l):
+                return self.b[o][l] + model.theta[o]*self.gb[l] == sum(model.lamda[o, r] * self.bref[r][l] for r in model.R)
+            return undesirable_output_rule
+
+    def __vrs_rule(self):
+        if self.__reference == False:
+            def vrs_rule(model, o):
+                return sum(model.lamda[o, i] for i in model.I) == 1
+            return vrs_rule
+        else:
+            def vrs_rule(model, o):
+                return sum(model.lamda[o, r] for r in model.R) == 1
+            return vrs_rule            
