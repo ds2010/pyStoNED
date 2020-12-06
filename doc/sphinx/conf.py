c9384cc9924b2e6fb9c1b47093623e648de2147e
Sheng Dai <sheng.dai@aalto.fi>
2020-12-06 21:03:40 +0200

    Update conf.py

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -13,9 +13,13 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../..'))
 
-on_rtd = os.environ.get('READTHEDOCS') == 'True'
+import mock
+
+MOCK_MODULES = ['numpy', 'scipy']
+for mod_name in MOCK_MODULES:
+   sys.modules[mod_name] = mock.Mock() 
 
 # -- Project information -----------------------------------------------------
 
