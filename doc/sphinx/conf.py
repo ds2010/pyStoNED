56c70093fba72eb0869c397d1c5a0ef8022f608e
Sheng Dai <sheng.dai@aalto.fi>
2020-12-06 21:23:26 +0200

    Update conf.py

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -13,13 +13,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../..'))
-
-import mock
-
-MOCK_MODULES = ['numpy', 'scipy', 'pyomo', 'pandas', 'matplotlib']
-for mod_name in MOCK_MODULES:
-   sys.modules[mod_name] = mock.Mock()    
+sys.path.insert(0, os.path.abspath('../../'))
    
 # -- Project information -----------------------------------------------------
 
