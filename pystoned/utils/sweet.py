6bfcac1be52b69893f07c03e24f37eca3688fc58
Sheng Dai <sheng.dai@aalto.fi>
2021-01-20 19:54:38 +0200

    remove unused code

--- pystoned/utils/sweet.py
+++ pystoned/utils/sweet.py
@@ -8,19 +8,12 @@ def sweet(x):
     df = np.asmatrix(x)
 
     # calculate distance matrix
-    distmax = 0.0
-    distmin = 1000000
-
     distance = np.zeros((len(df), len(df)))
     for i in range(len(df)):
         for j in range(len(df)):
             if i != j:
                 distance[i, j] = np.sqrt(
                     np.sum(np.square(df[i, :] - df[j, :])))
-            if distance[i, j] > distmax:
-                distmax = distance[i, j]
-            if distance[i, j] < distmin:
-                distmin = distance[i, j]
 
     # calculate distance cut
     distcut = np.asmatrix(np.percentile(distance, 3, axis=0))
