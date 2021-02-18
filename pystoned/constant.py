8438c7636e507c2dbf7489bf996d6ec6e78ad315
JulianATA <julianisabear@gmail.com>
2021-02-18 15:28:24 +0800

    feat(pystoNED): Unify configuration constants

---
+++ pystoned/constant.py
@@ -0,0 +1,23 @@
+# Composite error term
+CET_ADDI = "addi"
+CET_MULT = "mult"
+CET_Categories = {
+    CET_ADDI: "Additive composite error term",
+    CET_MULT: "Multiplicative composite error term"
+}
+
+# Frontier
+FUN_PROD = "prod"
+FUN_COST = "cost"
+FUN_Categories = {
+    FUN_PROD: "Production frontier",
+    FUN_COST: "Cost frontier"
+}
+
+# Return to scale
+RTS_VRS = "vrs"
+RTS_CRS = "crs"
+RTS_Categories = {
+    RTS_VRS: "Variable returns to scale",
+    RTS_CRS: "Constant returns to scale"
+}
