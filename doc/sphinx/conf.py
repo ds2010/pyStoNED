79d476f9d6926c97d79082963b815ac134bd0d87
Sheng Dai <sheng.dai@aalto.fi>
2021-03-12 20:33:18 +0200

    update to version 0.4.4

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -28,7 +28,7 @@ author = u'Sheng Dai, Yu-Hsueh Fang, Chia-Yen Lee, Kuosmanen Timo'
 # The short X.Y version.
 version = '0.4'
 # The full version, including alpha/beta/rc tags.
-release = '0.4.3'
+release = '0.4.4'
 
 # -- General configuration ---------------------------------------------------
 
