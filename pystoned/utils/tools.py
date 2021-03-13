1508f3834e6b8c761e2e6899ad995aa8c508d4ba
JulianATA <julianisabear@gmail.com>
2021-03-13 21:11:06 +0800

    feat(tools): Log optimizing locally

--- pystoned/utils/tools.py
+++ pystoned/utils/tools.py
@@ -5,6 +5,9 @@ __email_re = re.compile(r'([^@]+@[^@]+\.[a-zA-Z0-9]+)$')
 
 
 def set_neos_email(address):
+    if address == OPT_LOCAL:
+        print("Optimizing locally.")
+        return False
     if not __email_re.match(address):
         # TODO: Replace with log system
         print("Invalid email address.\n")
