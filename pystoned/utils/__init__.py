40c489c52a53300fa16357d727df51cf43ee2826
ds2010 <sheng.dai@aalto.fi>
2021-04-12 21:56:46 +0300

    feat(interpolation): add a new interpolation function

--- pystoned/utils/__init__.py
+++ pystoned/utils/__init__.py
@@ -6,5 +6,6 @@ from . import CQERG1
 from . import CQERG2
 from . import CQERZG1
 from . import CQERZG2
+from . import interpolation
 from . import sweet
 from . import tools
---
+++ pystoned/utils/interpolation.py
@@ -0,0 +1,32 @@
+# import dependencies
+import numpy as np
+from ..constant import FUN_PROD, FUN_COST
+
+
+def interpolation(alpha, beta, x, fun=FUN_PROD):
+    """Interpolate estimated function/frontier 
+
+    Args:
+        alpha (float): estimated alpha.
+        beta (float): estimated beta.
+        x (float): input variables.
+        fun (String, optional): FUN_PROD (production frontier) or FUN_COST (cost frontier). Defaults to FUN_PROD.
+
+    Returns:
+        yat: interpolated frontier
+    """
+    n = len(x)
+    d = len(x[0])
+
+    if fun == FUN_PROD:
+        def fun_est(x):
+                return min(alpha + np.sum(beta[:,0:d] * np.tile(x,(len(beta[:,0:d]), 1)), axis=1))
+    elif fun == FUN_COST:
+        def fun_est(x):
+                return max(alpha + np.sum(beta[:,0:d] * np.tile(x,(len(beta[:,0:d]), 1)), axis=1))
+
+    yhat = np.zeros((n, 1))
+    for i in range(n):
+        yhat[i, :] = fun_est(x[i, :])
+
+    return yhat
