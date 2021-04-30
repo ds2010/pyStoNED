a66cbf2255f65a760c0ca90e2c2886229b741c6a
Sheng Dai <dais2@Shengs-MBP.lan>
2021-04-30 11:44:47 +0300

    feat(ICQER.py): add the z variable to module

--- pystoned/ICQER.py
+++ pystoned/ICQER.py
@@ -8,18 +8,19 @@ class ICQR(ICNLS.ICNLS, CQER.CQR):
     """Isotonic convex quantile regression (ICQR)
     """
 
-    def __init__(self, y, x, tau, cet=CET_ADDI, fun=FUN_PROD, rts=RTS_VRS):
+    def __init__(self, y, x, tau, z=None, cet=CET_ADDI, fun=FUN_PROD, rts=RTS_VRS):
         """ICQR model
 
         Args:
              y (float): output variable. 
              x (float): input variables.
              tau (float): quantile.
+             z (float, optional): Contextual variable(s). Defaults to None.
              cet (String, optional): CET_ADDI (additive composite error term) or CET_MULT (multiplicative composite error term). Defaults to CET_ADDI.
              fun (String, optional): FUN_PROD (production frontier) or FUN_COST (cost frontier). Defaults to FUN_PROD.
              rts (String, optional): RTS_VRS (variable returns to scale) or RTS_CRS (constant returns to scale). Defaults to RTS_VRS.
         """
-        CQER.CQR.__init__(self, y, x, tau, cet, fun, rts)
+        CQER.CQR.__init__(self, y, x, tau, z, cet, fun, rts)
 
         self._ICNLS__pmatrix = self._ICNLS__binaryMatrix()
         self.__model__.afriat_rule.deactivate()
@@ -32,18 +33,19 @@ class ICQR(ICNLS.ICNLS, CQER.CQR):
 class ICER(ICNLS.ICNLS, CQER.CER):
     """Isotonic convex expectile regression (ICER)"""
 
-    def __init__(self, y, x, tau, cet=CET_ADDI, fun=FUN_PROD, rts=RTS_VRS):
+    def __init__(self, y, x, tau, z=None, cet=CET_ADDI, fun=FUN_PROD, rts=RTS_VRS):
         """ICER model
 
         Args:
              y (float): output variable. 
              x (float): input variables.
              tau (float): expectile.
+             z (float, optional): Contextual variable(s). Defaults to None.
              cet (String, optional): CET_ADDI (additive composite error term) or CET_MULT (multiplicative composite error term). Defaults to CET_ADDI.
              fun (String, optional): FUN_PROD (production frontier) or FUN_COST (cost frontier). Defaults to FUN_PROD.
              rts (String, optional): RTS_VRS (variable returns to scale) or RTS_CRS (constant returns to scale). Defaults to RTS_VRS.
         """
-        CQER.CER.__init__(self, y, x, tau, cet, fun, rts)
+        CQER.CER.__init__(self, y, x, tau, z, cet, fun, rts)
 
         self._ICNLS__pmatrix = self._ICNLS__binaryMatrix()
         self.__model__.afriat_rule.deactivate()
