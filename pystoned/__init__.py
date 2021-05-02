856486d38cd04fa60730c68cb51b6db9a9d535b9
JulianATA <julianisabear@gmail.com>
2021-05-02 18:10:02 +0800

    feat(plot): Modify input of plot to model

--- pystoned/__init__.py
+++ pystoned/__init__.py
@@ -8,7 +8,7 @@ from . import CQERG
 from . import dataset
 from . import DEA
 from . import FDH
-from . import Plot
+from . import plot
 from . import ICNLS
 from . import ICQER
 from . import StoNED
@@ -24,7 +24,7 @@ __all__ = [
     'dataset',
     'DEA',
     'FDH',
-    'Plot',
+    'plot',
     'ICNLS',
     'ICQER',
     'StoNED',
