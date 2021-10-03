b76ca090841690085e659d3d4e73ee2c05df4158
Dai Sheng <sheng.dai@aalto.fi>
2021-10-03 12:54:43 +0300

    add dual models to DEA module

--- pystoned/DEA.py
+++ pystoned/DEA.py
@@ -2,7 +2,7 @@
 from pyomo.environ import ConcreteModel, Set, Var, Objective, minimize, maximize, Constraint
 import numpy as np
 import pandas as pd
-from .constant import CET_ADDI, ORIENT_IO, ORIENT_OO, RTS_VRS, OPT_DEFAULT, OPT_LOCAL
+from .constant import CET_ADDI, ORIENT_IO, ORIENT_OO, RTS_VRS, RTS_CRS, OPT_DEFAULT, OPT_LOCAL
 from .utils import tools
 
 
@@ -11,7 +11,7 @@ class DEA:
     """
 
     def __init__(self, y, x, orient, rts, yref=None, xref=None):
-        """DEA model
+        """DEA: Envelopment problem 
 
         Args:
             y (float): output variable. 
@@ -28,13 +28,13 @@ class DEA:
         self.y, self.x = tools.assert_valid_mupltiple_y_data(y, x)
         self.orient = orient
         self.rts = rts
-        self.__reference = False
 
         if type(yref) != type(None):
-            self.__reference = True
             self.yref, self.xref = tools.assert_valid_reference_data(
                 self.y, self.x, yref, xref)
-            self.__model__.R = Set(initialize=range(len(self.yref)))
+        else:
+            self.yref, self.xref = self.y, self.x
+        self.__model__.R = Set(initialize=range(len(self.yref)))
 
         # Initialize sets
         self.__model__.I = Set(initialize=range(len(self.y)))
@@ -75,55 +75,30 @@ class DEA:
 
     def __input_rule(self):
         """Return the proper input constraint"""
-        if self.__reference == False:
-            if self.orient == ORIENT_IO:
-                def input_rule(model, o, j):
-                    return model.theta[o]*self.x[o][j] >= sum(model.lamda[o, i]*self.x[i][j] for i in model.I)
-                return input_rule
-            elif self.orient == ORIENT_OO:
-                def input_rule(model, o, j):
-                    return sum(model.lamda[o, i] * self.x[i][j] for i in model.I) <= self.x[o][j]
-                return input_rule
-        else:
-            if self.orient == ORIENT_IO:
-                def input_rule(model, o, j):
-                    return model.theta[o]*self.x[o][j] >= sum(model.lamda[o, r]*self.xref[r][j] for r in model.R)
-                return input_rule
-            elif self.orient == ORIENT_OO:
-                def input_rule(model, o, j):
-                    return sum(model.lamda[o, r] * self.x[r][j] for r in model.I) <= self.x[o][j]
-                return input_rule
+        if self.orient == ORIENT_IO:
+            def input_rule(model, o, j):
+                return model.theta[o]*self.x[o][j] >= sum(model.lamda[o, r]*self.xref[r][j] for r in model.R)
+            return input_rule
+        elif self.orient == ORIENT_OO:
+            def input_rule(model, o, j):
+                return sum(model.lamda[o, r] * self.x[r][j] for r in model.I) <= self.x[o][j]
+            return input_rule
 
     def __output_rule(self):
         """Return the proper output constraint"""
-        if self.__reference == False:
-            if self.orient == ORIENT_IO:
-                def output_rule(model, o, k):
-                    return sum(model.lamda[o, i] * self.y[i][k] for i in model.I) >= self.y[o][k]
-                return output_rule
-            elif self.orient == ORIENT_OO:
-                def output_rule(model, o, k):
-                    return model.theta[o]*self.y[o][k] <= sum(model.lamda[o, i]*self.y[i][k] for i in model.I)
-                return output_rule
-        else:
-            if self.orient == ORIENT_IO:
-                def output_rule(model, o, k):
-                    return sum(model.lamda[o, r] * self.yref[r][k] for r in model.R) >= self.y[o][k]
-                return output_rule
-            elif self.orient == ORIENT_OO:
-                def output_rule(model, o, k):
-                    return model.theta[o]*self.y[o][k] <= sum(model.lamda[o, r]*self.y[r][k] for r in model.R)
-                return output_rule
+        if self.orient == ORIENT_IO:
+            def output_rule(model, o, k):
+                return sum(model.lamda[o, r] * self.yref[r][k] for r in model.R) >= self.y[o][k]
+            return output_rule
+        elif self.orient == ORIENT_OO:
+            def output_rule(model, o, k):
+                return model.theta[o]*self.y[o][k] <= sum(model.lamda[o, r]*self.y[r][k] for r in model.R)
+            return output_rule
 
     def __vrs_rule(self):
-        if self.__reference == False:
-            def vrs_rule(model, o):
-                return sum(model.lamda[o, i] for i in model.I) == 1
-            return vrs_rule
-        else:
-            def vrs_rule(model, o):
-                return sum(model.lamda[o, r] for r in model.R) == 1
-            return vrs_rule
+        def vrs_rule(model, o):
+            return sum(model.lamda[o, r] for r in model.R) == 1
+        return vrs_rule
 
     def optimize(self, email=OPT_LOCAL, solver=OPT_DEFAULT):
         """Optimize the function by requested method
@@ -170,7 +145,7 @@ class DEA:
         return lamda.to_numpy()
 
 
-class DEADDF(DEA):
+class DDF(DEA):
     def __init__(self,  y, x, b=None, gy=[1], gx=[1], gb=None, rts=RTS_VRS, yref=None, xref=None, bref=None):
         """DEA DDF model 
 
@@ -296,4 +271,148 @@ class DUAL(DEA):
         """
         # Initialize DEA model
         self.__model__ = ConcreteModel()
+
+        self.y, self.x = tools.assert_valid_mupltiple_y_data(y, x)
+        self.orient = orient
+        self.rts = rts
+
+        if type(yref) != type(None):
+            self.yref, self.xref = tools.assert_valid_reference_data(
+                self.y, self.x, yref, xref)
+        else:
+            self.yref, self.xref = self.y, self.x
+        self.__model__.R = Set(initialize=range(len(self.yref)))
+
+        # Initialize sets
+        self.__model__.I = Set(initialize=range(len(self.y)))
+        self.__model__.J = Set(initialize=range(len(self.x[0])))
+        self.__model__.K = Set(initialize=range(len(self.y[0])))
+
+        # Initialize variable
+        self.__model__.nu = Var(self.__model__.I, self.__model__.J, bounds=(
+            0.0, None), doc='multiplier x')
+        self.__model__.mu = Var(self.__model__.I, self.__model__.K, bounds=(
+            0.0, None), doc='multiplier y')
+        if self.rts == RTS_VRS:
+            self.__model__.omega = Var(self.__model__.I, doc='variable return to scale')
+
+        # Setup the objective function and constraints
+        if self.orient == ORIENT_IO:
+            self.__model__.objective = Objective(
+                rule=self.__objective_rule(), sense=maximize, doc='objective function')
+        else:
+            self.__model__.objective = Objective(
+                rule=self.__objective_rule(), sense=minimize, doc='objective function')
+        self.__model__.first = Constraint(
+            self.__model__.I, self.__model__.R, rule=self.__first_rule(), doc='technology constraint')
+        self.__model__.second = Constraint(
+            self.__model__.I, rule=self.__second_rule(), doc='normalization constraint')
+
+        # Optimize model
+        self.optimization_status = 0
+        self.problem_status = 0
+
+    def __objective_rule(self):
+        """Return the proper objective function"""
+        if self.orient == ORIENT_IO:
+            def objective_rule(model):
+                if self.rts == RTS_VRS:
+                    return sum(sum(model.mu[o, k] * self.y[o][k] for o in model.I) for k in model.K) + sum(model.omega[o] for o in model.I)
+                elif self.rts == RTS_CRS: 
+                    return sum(sum(model.mu[o, k] * self.y[o][k] for o in model.I) for k in model.K)
+            return objective_rule
+        elif self.orient == ORIENT_OO:
+            def objective_rule(model):
+                if self.rts == RTS_VRS:
+                    return sum(sum(model.nu[o, j] * self.x[o][j] for o in model.I) for j in model.J) + sum(model.omega[o] for o in model.I)
+                elif self.rts == RTS_CRS: 
+                    return sum(sum(model.nu[o, j] * self.x[o][j] for o in model.I) for j in model.J)
+            return objective_rule            
+ 
+    def __first_rule(self):
+        """Return the proper technology constraint"""
+        if self.orient == ORIENT_IO:
+            if self.rts == RTS_VRS:
+                def first_rule(model, o, r):
+                    return sum(model.mu[o, k] * self.yref[r][k] for k in model.K) - sum(model.nu[o, j] * self.xref[r][j] for j in model.J) + model.omega[o] <= 0
+                return first_rule
+            elif self.rts == RTS_CRS:
+                def first_rule(model, o, r):
+                    return sum(model.mu[o, k] * self.yref[r][k] for k in model.K) - sum(model.nu[o, j] * self.xref[r][j] for j in model.J)  <= 0
+                return first_rule         
+        elif self.orient == ORIENT_OO:
+            if self.rts == RTS_VRS:
+                def first_rule(model, o, r):
+                    return sum(model.nu[o, j] * self.xref[r][j] for j in model.J) - sum(model.mu[o, k] * self.yref[r][k] for k in model.K) + model.omega[o] >= 0
+                return first_rule
+            elif self.rts == RTS_CRS:
+                def first_rule(model, o, r):
+                    return sum(model.nu[o, j] * self.xref[r][j] for j in model.J) - sum(model.mu[o, k] * self.yref[r][k] for k in model.K) >= 0
+                return first_rule      
+
+    def __second_rule(self):
+        """Return the proper normalization constraint"""
+        if self.orient == ORIENT_IO:
+            def second_rule(model, o):
+                return sum(model.nu[o, j] * self.x[o][j] for j in model.J) == 1
+            return second_rule
+        elif self.orient == ORIENT_OO:
+            def second_rule(model, o):
+                return sum(model.mu[o, k] * self.y[o][k] for k in model.K) == 1
+            return second_rule
+
+    def display_mu(self):
+        """Display mu value"""
+        tools.assert_optimized(self.optimization_status)
+        self.__model__.mu.display()
+
+    def display_nu(self):
+        """Display nu value"""
+        tools.assert_optimized(self.optimization_status)
+        self.__model__.nu.display()
+
+    # Omega only exists in VRS model. So, print a similar info like: https://github.com/ds2010/pyStoNED/blob/dfdddd9e143933880ac1444dfd895157f8277122/pystoned/utils/tools.py#L202
+    def display_omega(self):
+         """Display omega value"""
+         tools.assert_optimized(self.optimization_status)
+         self.__model__.omega.display()       
+
+    def get_mu(self):
+        """Return mu value by array"""
+        tools.assert_optimized(self.optimization_status)
+        mu = np.asarray([i + tuple([j]) for i, j in zip(list(self.__model__.mu),
+                                                          list(self.__model__.mu[:, :].value))])
+        mu = pd.DataFrame(mu, columns=['Name', 'Key', 'Value'])
+        mu = mu.pivot(index='Name', columns='Key', values='Value')
+        return mu.to_numpy()
+
+    def get_nu(self):
+        """Return nu value by array"""
+        tools.assert_optimized(self.optimization_status)
+        nu = np.asarray([i + tuple([j]) for i, j in zip(list(self.__model__.nu),
+                                                          list(self.__model__.nu[:, :].value))])
+        nu = pd.DataFrame(nu, columns=['Name', 'Key', 'Value'])
+        nu = nu.pivot(index='Name', columns='Key', values='Value')
+        return nu.to_numpy()
+
+    # SEE above
+    def get_omega(self):
+        """Return omega value by array"""
+        tools.assert_optimized(self.optimization_status)
+        omega = list(self.__model__.omega[:].value)
+        return np.asarray(omega)
+
+    def get_efficiency(self):
+        """Return efficiency value by array"""
+        tools.assert_optimized(self.optimization_status)
+        if self.orient == ORIENT_IO:
+            if self.rts == RTS_CRS:
+                return self.get_mu()*self.y
+            elif self.rts == RTS_VRS:
+                return self.get_mu()*self.y + self.get_omega().reshape(len(self.y), 1)
+        elif self.orient == ORIENT_OO:
+            if self.rts == RTS_CRS:
+                return (np.sum(self.get_nu()*self.x, axis=1)).reshape(len(self.x), 1)
+            elif self.rts == RTS_VRS:
+                return (np.sum(self.get_nu()*self.x, axis=1)).reshape(len(self.x), 1) + self.get_omega().reshape(len(self.x), 1)
 
