532d7d67a5c74cd700c9381f59dfb742a91821ce
Sheng Dai <dais2@dyn38-79.vpn.net.utu.fi>
2023-12-27 15:09:09 +0800

    update to v0.7.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as readme_file:
 
 setup_args = dict(
     name='pystoned',
-    version='0.7.1',
+    version='0.7.2',
     description='A Python Package for Convex Regression and Frontier Estimation',
     long_description_content_type="text/markdown",
     long_description=README,
